15a7e251876b0de3312fd93453d4f2cae98ce4bb
ea <entreaccolades@gmail.com>
2020-07-16 14:31:12 +0200

    type: App desc: add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Routes from './routes';
 
-function App() {
+const Layout = ({ children }) => {
   return (
     <>
-      <header>
-      </header>
+      <Header />
+      <main>
+        {children}
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
+
+
+const MapRoutes = () =>(
+  <Layout>
+    <Switch>
+      {Routes.map(({ component, path }, k) => <Route key={k} component={component} path={path} exact />)}
+    </Switch>
+  </Layout>
+);
+
+const App =  () => <Router><Route component={MapRoutes} path="/" /></Router>
 
 export default App;
