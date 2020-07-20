b1fcc29058557cee83512422f1424664fb20d26d
ea <entreaccolades@gmail.com>
2020-07-20 17:48:26 +0200

    :lipstick: add global style application

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
+import { global } from '../src/utils/styles/globalStyle';
 import App from './App';
+
+const GlobalStyle = createGlobalStyle`${global}`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 
