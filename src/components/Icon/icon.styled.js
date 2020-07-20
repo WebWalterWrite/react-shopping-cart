cd513fe6c11aa037c7fde81dc6564e43007afb16
ea <entreaccolades@gmail.com>
2020-07-20 17:47:45 +0200

    :lipstick: update style Icon component

--- src/components/Icon/icon.styled.js
+++ src/components/Icon/icon.styled.js
@@ -4,6 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const IconStyled = styled(FontAwesomeIcon)`
   color: ${props => props.color || "#f6f6f5"};
-  font-size: ${props => `${props.size}em`};
+  font-size: ${props => `${props.font}em`};
 `;
 
