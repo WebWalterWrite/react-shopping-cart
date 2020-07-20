2a95bc66dc9c8d2e97c15f94debdc3c84dc55f7f
ea <entreaccolades@gmail.com>
2020-07-20 17:37:34 +0200

    type: component desc: correction erreur attributs size changer en font suite erreur Invalid value used in size prop

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -2,11 +2,19 @@ import React from 'react'
 import { IconStyled } from './icon.styled';
 import icons from '../../utils/styles/icons/icons.fontawesome';
 
+/**
+ * @description - Cr&er un fontawesome icon à partir des props
+ * @param {string} name -Nom du boutton
+ * @param {string} color - Couleur de fond
+ * @param {string} size -  Taille icon
+ */
 const Icon = ({ name, color, size }) => {
 
   return(
-    <IconStyled icon={icons[name]} color={color} size={size} />
+    <IconStyled icon={icons[name]} color={color} font={size} />
   )
 };
+
+
 
 export default Icon;
